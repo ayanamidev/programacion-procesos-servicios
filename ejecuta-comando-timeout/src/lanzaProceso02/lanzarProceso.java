9794ad04be5ad610f287344713889cbbed39581a
LauraDev <lauralodeirodev@gmail.com>
2024-09-24 10:03:37 +0200

    first commit

---
+++ ejecuta-comando-timeout/src/lanzaProceso02/lanzarProceso.java
@@ -0,0 +1,42 @@
+package lanzaProceso02;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class lanzarProceso {
+	
+	public static int MAX_TIEMPO=500;
+	
+	public static void main(String[] args) {
+		
+		ProcessBuilder pb=new ProcessBuilder(new String[] {"notepad.exe"});
+		
+		System.out.printf("Se ejecuta comando:%s\n",Arrays.toString(args));
+		
+		pb.inheritIO();
+		pb.redirectErrorStream(true);
+		
+		try {
+			Process p=pb.start();
+			if(!p.waitFor(MAX_TIEMPO, TimeUnit.MILLISECONDS)) {
+				p.destroy();
+				System.out.printf("AVISO: no ha terminado en %d ms\n",MAX_TIEMPO);
+			}
+			
+		} catch (IOException e) {
+			System.err.println("Error durante la ejecucion. Infromacion detallada");
+			System.err.println("--------------------");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.err.println("--------------------");
+			System.exit(1);
+			
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Proceso interrumpido");
+			System.exit(2);
+		}
+	}
+}
