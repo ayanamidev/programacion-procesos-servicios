d61adb641804c3dd40b78e355940a08ce205a754
LauraDev <lauralodeirodev@gmail.com>
2024-10-02 09:46:02 +0200

    Elimino carpeta que no es de esta asignatura y añado nuevo ejercciio de hilos

---
+++ hilos-cooperantes-no-sincronizados/hilos-cooperantes-no-sincronizados/src/Hilo.java
@@ -0,0 +1,17 @@
+public class Hilo implements Runnable{
+
+    int numHilo, miParte, miCuenta=0;
+    private final Contador cont;
+
+    public int getMiCuenta(){
+        
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+    
+    
+}
